a0d9d0ecaf36616921364bdd0977d92119910009
vijend795 <107410066+vijend795@users.noreply.github.com>
2023-10-13 20:15:31 +1300

    not using database for vercel so comment out

--- GoNZProject/GoNZProject/settings.py
+++ GoNZProject/GoNZProject/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = 'django-insecure-jdcc2a4#w-7aj-t+=$i!5a9#kvvba7cw=uutkiw7=kwg1i&6gf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -94,12 +94,12 @@ WSGI_APPLICATION = 'GoNZProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
